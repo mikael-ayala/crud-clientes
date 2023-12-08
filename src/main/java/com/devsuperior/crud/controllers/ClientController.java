650bcd0a26711956cd6c25ec7ec3feb6aa5e19fd
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-12-08 14:50:39 -0300

    add findAll pageable

--- src/main/java/com/devsuperior/crud/controllers/ClientController.java
+++ src/main/java/com/devsuperior/crud/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.crud.controllers;
 import com.devsuperior.crud.dto.ClientDTO;
 import com.devsuperior.crud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,12 @@ public class ClientController {
 
     @Autowired
     private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> clients = clientService.findAll(pageable);
+        return ResponseEntity.ok(clients);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
--- src/main/java/com/devsuperior/crud/services/ClientService.java
+++ src/main/java/com/devsuperior/crud/services/ClientService.java
@@ -4,15 +4,25 @@ import com.devsuperior.crud.dto.ClientDTO;
 import com.devsuperior.crud.entities.Client;
 import com.devsuperior.crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository clientRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> page = clientRepository.findAll(pageable);
+        return page.map(ClientDTO::new);
+    }
 
     @Transactional(readOnly = true)
     public ClientDTO findById(@PathVariable Long id) {
